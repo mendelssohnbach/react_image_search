4b52c4ce36556bfe313c93045976461f1292b333
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2023-10-02 16:09:02 +0900

    Initial Search UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react';
 import './index.css';
+import { Form } from 'react-bootstrap';
 
-function App() {
-  return <div>Welcome to Unsplash Image Search</div>;
-}
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Image Search</h1>
+      <div className="search-section">
+        <Form>
+          <Form.Control
+            type="search"
+            placeholder="Type something to search..."
+            className="search-input"
+          />
+        </Form>
+      </div>
+    </div>
+  );
+};
 
 export default App;
