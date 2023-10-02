2129724a289d5451045f3a442bb23a8e664be12e
mendelssohnbach <mendelssohnusaic+github@gmail.com>
2023-10-03 06:38:06 +0900

    Display images on screen

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import './index.css';
 import { Form } from 'react-bootstrap';
+import axios from 'axios';
+
+const API_URL = 'https://api.unsplash.com/search/photos';
+const IMAGES_PER_PAGE = 20;
 
 function App() {
   const searchInput = useRef(null);
+  const [images, setImages] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const fetchImages = async () => {
+    try {
+      const { data } = await axios.get(
+        `${API_URL}?query=${
+          searchInput.current.value
+        }&page=1&per_page=${IMAGES_PER_PAGE}&client_id=${import.meta.env.VITE_API_KEY}`
+      );
+      console.log('data', data);
+      setImages(data.results);
+      setTotalPages(data.total_pages);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   const handleSearch = (event) => {
     event.preventDefault();
     console.log(searchInput.current.value);
+    fetchImages();
   };
 
   const handleSelection = (selection) => {
     searchInput.current.value = selection;
+    fetchImages();
   };
 
   return (
@@ -32,6 +55,16 @@ function App() {
         <div onClick={() => handleSelection('birds')}>Birds</div>
         <div onClick={() => handleSelection('cats')}>Cats</div>
         <div onClick={() => handleSelection('shoes')}>Shoes</div>
+      </div>
+      <div className="images">
+        {images.map((image) => (
+          <img
+            key={image.id}
+            src={image.urls.small}
+            alt={image.alt_description}
+            className="image"
+          />
+        ))}
       </div>
     </div>
   );
